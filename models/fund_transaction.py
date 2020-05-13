05fa4c84afe024d9e418f751464536a6c15ca93f
Carlos <cpinelly@gmail.com>
2020-05-13 14:30:56 -0500

    Assigment field for fund transaction.

--- models/fund_transaction.py
+++ models/fund_transaction.py
@@ -5,4 +5,5 @@ from models.fund import Fund
 class FundTransaction(mongoengine.EmbeddedDocument):
     fund = mongoengine.ReferenceField(Fund, required=True)
     change = mongoengine.FloatField(required=True)
+    assigment = mongoengine.FloatField(required=True)
     balance = mongoengine.FloatField(required=True)
