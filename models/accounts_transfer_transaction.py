1b5b2212b0afe738d8a74df0770f2280cbcb1bc7
Carlos <cpinelly@gmail.com>
2020-06-10 22:29:32 -0500

    Finx for fund transfer transactions class

--- models/accounts_transfer_transaction.py
+++ models/accounts_transfer_transaction.py
@@ -9,18 +9,19 @@ class AccountsTransferTransaction(Transaction):
 
     def __init__(self, **kwargs):
 
-        if not 'amount' in kwargs or not 'to_account_id' in kwargs or not 'from_account_id' in kwargs :
-            raise mongoengine.ValidationError("Missing parameters")
-
-        amount = kwargs.pop('amount')
-        to_account_id = kwargs.pop('to_account_id')
-        from_account_id = kwargs.pop('from_account_id')
-
-        if amount == 0:
-            raise mongoengine.ValidationError("Amount must be different than zero")
+        amount = kwargs.pop('amount') if 'amount' in kwargs else None
+        to_account_id = kwargs.pop('to_account_id') if 'to_account_id' in kwargs else None
+        from_account_id = kwargs.pop('from_account_id') if 'from_account_id' in kwargs else None
 
         super().__init__(**kwargs)
-        from_account_transaction = AccountTransaction(account=from_account_id, change=-amount)
-        to_account_transaction = AccountTransaction(account=to_account_id, change=amount)
-        self.account_transactions.append(from_account_transaction)
-        self.account_transactions.append(to_account_transaction)
+
+        if amount and to_account_id and from_account_id:
+
+            if amount == 0:
+                raise mongoengine.ValidationError("Amount must be different than zero")
+
+
+            from_account_transaction = AccountTransaction(account=from_account_id, change=-amount)
+            to_account_transaction = AccountTransaction(account=to_account_id, change=amount)
+            self.account_transactions.append(from_account_transaction)
+            self.account_transactions.append(to_account_transaction)
