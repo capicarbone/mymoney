9f94adf3ae75b3b52bd9b379b9a86c1eaa03582a
Carlos <cpinelly@gmail.com>
2020-06-12 15:45:26 -0500

    Test for invalid inputs for funds.

--- tests/unit/test_fund.py
+++ tests/unit/test_fund.py
@@ -30,11 +30,21 @@ def test_insert_with_total_percentage_greater_than_1(db, mongodb, user):
     with pytest.raises(mongoengine.ValidationError):
         f.save()
 
+@pytest.mark.parametrize( ('attr'), [
+    {'percentage_assigment': 0.1},
+    {'name': 'Test fund', 'percentage_assigment': 0.1, 'minimum_limit': 500, 'maximum_limit': 100}
+])
+def test_invalid_attributes_insert(db, mongodb, attr):
+    attr['owner'] = User.objects.first()
+    f = Fund(**attr)
+    with pytest.raises(mongoengine.ValidationError):
+        f.save()
+
 def test_valid_insert(db, mongodb):
     attr = {'name': 'Test Fund','percentage_assigment': 0.1, 'owner': User.objects.first() }
     f = Fund(**attr)
     f.save()
-    assert f.id
+    assert Fund.objects(name=attr["name"]).get() is not None
 
 
 
