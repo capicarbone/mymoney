1f9b94e594db6dfee91340af7827f3c94a2ccc21
Carlos <cpinelly@gmail.com>
2020-06-28 23:37:48 -0500

    Setup for work with tests in docker containers.

--- readme.md
+++ readme.md
@@ -3,3 +3,9 @@
 ```bash
 docker exec -it mymoney flask create-user <name> <email> <password>
 ```
+
+## Run unit tests
+
+```bash
+sudo docker exec -it mymoney pytest ./tests/unit
+```
--- tests/unit/fixtures.py
+++ tests/unit/fixtures.py
@@ -4,8 +4,9 @@ import os
 from models.user import User
 
 @pytest.fixture(scope="module")
-def db():
-    mongoengine.connect('mymoney_test', host=os.environ.get('MONGODB_TEST_URI', 'mongodb://localhost/mymoney_test'))
+def db(pytestconfig):
+    #mongoengine.connect('mymoney_test', host=os.environ.get('MONGODB_TEST_URI', 'mongodb://localhost/mymoney_test'))
+    mongoengine.connect('mymoney_test', host=pytestconfig.getini('mongodb_host'))
     return mongoengine.get_connection()
 
 @pytest.fixture()
