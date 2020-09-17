d021f6e39b4a13cb61684fa5112991d62c11f962
Carlos <cpinelly@gmail.com>
2020-09-16 21:17:41 -0500

    Fix

--- models/category.py
+++ models/category.py
@@ -13,6 +13,6 @@ class TransactionCategory(mongoengine.Document):
     kind = mongoengine.StringField(choices=CATEGORY_KINDS, default=CATEGORY_KINDS[0], required=True)
     created_at = mongoengine.DateTimeField(default=lambda: datetime.datetime.now())
 
-    def is_income(self):
+    def is_income(self) -> bool:
         return self.kind == 'income'
 
