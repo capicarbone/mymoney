c73201029227e2e795478acef3e3ad91efbf54d8
Carlos Pinelly <cpinelly@gmail.com>
2021-02-23 17:26:55 -0500

    Basic test for accounts resources.

--- tests/functional/test_accounts_resource.py
+++ tests/functional/test_accounts_resource.py
@@ -1,6 +1,40 @@
 
 from .fixtures import *
 
-def test_accounts_get(client, mongodb):
-    rv = client.get('/api/accounts', headers={'Authorization': 'Bearer 4427db92-e12f-4dbd-a8d2-46bb3ad85afe'})
+resource_url = '/api/accounts'
+
+def test_get_accounts_as_success(client, authenticated_header, mongodb):
+    rv = client.get(resource_url, headers=authenticated_header)
     assert rv.status_code == 200
+
+    json_data = rv.get_json()
+    assert type(json_data) is list
+    assert len(json_data) > 0
+    for e in json_data:
+        assert 'id' in e
+        assert 'name' in e
+        assert 'balance' in e
+
+
+def test_post_account_as_success(client, authenticated_header, mongodb):
+    rv = client.post(resource_url, headers=authenticated_header, json={'name': 'New Account'})
+    assert rv.status_code == 200
+
+    json_data = rv.get_json()
+    assert 'id' in json_data
+
+def test_post_account_invalid(client, authenticated_header, mongodb):
+    rv = client.post(resource_url, headers=authenticated_header, json={})
+    assert rv.status_code == 400
+
+    rv = client.post(resource_url, headers=authenticated_header, json={'name': ""})
+    assert rv.status_code == 400
+
+def test_account_unauthorizred_request(client, mongodb):
+    rv = client.get(resource_url)
+    assert rv.status_code == 401
+
+    rv = client.post(resource_url, json={'name': 'New Account'})
+    assert rv.status_code == 401
+
+
