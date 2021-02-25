1eaa461bc72e2f469d9ae109d20736808a1bb6ed
Carlos Pinelly <cpinelly@gmail.com>
2021-02-25 18:21:00 -0500

    Tests for authentication.

---
+++ tests/functional/test_authentication.py
@@ -0,0 +1,32 @@
+import base64
+from .fixtures import *
+
+
+def test_valid_login(client, mongodb):
+    credentials_base64 = base64.urlsafe_b64encode(b"cpinelly@gmail.com:555555")
+
+    header = {'Authorization': 'Basic %s' % credentials_base64.decode("utf-8")}
+
+    rv = client.post('/api/login', headers=header)
+
+    assert rv.status_code == 200
+    assert type(rv.get_json()) is dict
+    assert 'token' in rv.get_json()
+
+
+def test_invalid_login(client, mongodb):
+    credentials_base64 = base64.urlsafe_b64encode(b"c@gmail.com:555555")
+
+    header = {'Authorization': 'Basic %s' % credentials_base64.decode("utf-8")}
+
+    rv = client.post('/api/login', headers=header)
+
+    assert rv.status_code == 401
+
+    credentials_base64 = base64.urlsafe_b64encode(b"cpinelly@gmail.com:2323")
+
+    header = {'Authorization': 'Basic %s' % credentials_base64.decode("utf-8")}
+
+    rv = client.post('/api/login', headers=header)
+
+    assert rv.status_code == 401
