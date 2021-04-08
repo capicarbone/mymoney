5d2525caeb0cef8057b36c6c06245c8f74bbe8b7
Carlos Pinelly <cpinelly@gmail.com>
2021-04-08 16:00:14 -0500

    Added test for check accounts and funds balance equality. Fixtures fixes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,24 @@
 import pytest
 from models.category import TransactionCategory
 from models.account import Account
+from models.fund import Fund
 
-owner_id = "5ec7411b8741c3f6e1f07cea"
+@pytest.fixture()
+def main_user_id(mongodb):
+    return "5ec7411b8741c3f6e1f07cea"
 
 @pytest.fixture()
 def income_category(mongodb):
     return TransactionCategory.objects(id='5f60d51cc22a5d685b27bfe4').get()
 
 @pytest.fixture()
-def accounts(mongodb):
-    return Account.objects(owner=owner_id).all()
+def accounts(main_user_id):
+    return Account.objects(owner=main_user_id).all()
 
 @pytest.fixture()
-def expense_categories(mongodb):
-    return TransactionCategory.objects(owner=owner_id, kind="expense").all()
+def funds(main_user_id):
+    return Fund.objects(owner=main_user_id).all()
+
+@pytest.fixture()
+def expense_categories(main_user_id):
+    return TransactionCategory.objects(owner=main_user_id, kind="expense").all()
--- tests/unit/test_funds_assigment.py
+++ tests/unit/test_funds_assigment.py
@@ -1,14 +1,17 @@
-from models.category import TransactionCategory
+
+import random
+import datetime
 import pytest
 from decimal import Decimal
 from utils import fund_utils
 from models.fund import Fund
 from models.account import Account
 from models.income_transaction import IncomeTransaction
-import datetime
+from models.expense_transaction import ExpenseTransaction
+from models.category import TransactionCategory
 
 @pytest.fixture()
-def owner_id():
+def empty_user_id():
     return "5ee24ef16c8e3ad070cbf919"
 
 def test_fund_assignments_for_expense(db, mongodb, user):
@@ -65,7 +68,7 @@ def test_fund_assignments_for_expense(db, mongodb, user):
         'Unassigned': Decimal(0.01).quantize(Decimal("1.00"))
     })
 ])
-def test_fund_assignments_for_income_with_funds_on_deficit_and_change_is_not_enough_for_total_deficit(db, mongodb, owner_id, change, expected_assignments):
+def test_fund_assignments_for_income_with_funds_on_deficit_and_change_is_not_enough_for_total_deficit(db, mongodb, empty_user_id, change, expected_assignments):
     """
     The test is rand with a user witout any transaction and funds on deficit.
     :param db:
@@ -73,7 +76,7 @@ def test_fund_assignments_for_income_with_funds_on_deficit_and_change_is_not_eno
     :return:
     """
 
-    funds = Fund.objects(owner=owner_id)
+    funds = Fund.objects(owner=empty_user_id)
     change = Decimal(change)
 
     transactions = fund_utils.create_assignments_for_income(funds, change, datetime.date.today())
@@ -111,15 +114,15 @@ def test_fund_assignments_for_income_with_funds_on_deficit_and_change_is_not_eno
     }),
 
 ])
-def test_fund_assignments_for_income_with_funds_on_deficit_and_change_is_grather_than_total_deficit(db, mongodb, owner_id, change, expected_assignments):
+def test_fund_assignments_for_income_with_funds_on_deficit_and_change_is_grather_than_total_deficit(db, mongodb, empty_user_id, change, expected_assignments):
 
     # I add a transaction to force deficit on some funds
-    account = Account.objects(owner=owner_id, name="Banco").get()
+    account = Account.objects(owner=empty_user_id, name="Banco").get()
     category = TransactionCategory.objects(kind="income")[0]
-    income = IncomeTransaction(owner=owner_id, account_id=account, category=category,change=7000, date_accomplished=datetime.date.today())
+    income = IncomeTransaction(owner=empty_user_id, account_id=account, category=category,change=7000, date_accomplished=datetime.date.today())
     income.save()
 
-    funds = Fund.objects(owner=owner_id)
+    funds = Fund.objects(owner=empty_user_id)
     change = Decimal(change)
 
     transactions = fund_utils.create_assignments_for_income(funds, change, datetime.date.today())
@@ -154,15 +157,15 @@ def test_fund_assignments_for_income_with_funds_on_deficit_and_change_is_grather
         'Unassigned': Decimal(17500).quantize(Decimal("1.00"))
     })
 ])
-def test_fund_assignments_for_income_with_any_fund_without_deficit(db, mongodb, owner_id, change, expected_assignments):
+def test_fund_assignments_for_income_with_any_fund_without_deficit(db, mongodb, empty_user_id, change, expected_assignments):
 
     # I add a big transaction to force not deficit in any fund
-    account = Account.objects(owner=owner_id, name="Banco").get()
+    account = Account.objects(owner=empty_user_id, name="Banco").get()
     category = TransactionCategory.objects(kind="income")[0]
-    income = IncomeTransaction(owner=owner_id, category=category, account_id=account, change=14000, date_accomplished=datetime.date.today())
+    income = IncomeTransaction(owner=empty_user_id, category=category, account_id=account, change=14000, date_accomplished=datetime.date.today())
     income.save()
 
-    funds = Fund.objects(owner=owner_id)
+    funds = Fund.objects(owner=empty_user_id)
     change = Decimal(change)
 
     transactions = fund_utils.create_assignments_for_income(funds, change, datetime.date.today())
@@ -177,3 +180,54 @@ def test_fund_assignments_for_income_with_any_fund_without_deficit(db, mongodb, 
         assert assignment.change == expected_assignments[fund.name], "Fund {} with not expected assignment".format(fund.name)
 
     assert sum([t.change for t in funds_assigned]) == Decimal(change).quantize(Decimal("1.00"))
+
+@pytest.fixture()
+def simulate_real_transactions(db, accounts, expense_categories, income_category, main_user_id):
+
+    random.seed(2)
+    current_day = datetime.date(year=2016, month=3, day=1)
+    end_date = datetime.date(year=2021, month=1, day=1)
+
+    payday = 26
+    jobpayment = 2200
+    jobpayment_account = accounts[0]
+    last_payment_date = None
+
+    while current_day != end_date:
+        if current_day.day == payday and last_payment_date != current_day:
+            income = IncomeTransaction(
+                owner=main_user_id,
+                date_accomplished=current_day,
+                account_id=jobpayment_account.id,
+                change=jobpayment + random.random() * random.choice([1, 10, 100, 1000]),
+                category=income_category
+            )
+            income.save()
+            last_payment_date = datetime.date(year=current_day.year,
+                                              month=current_day.month,
+                                              day=current_day.day)
+
+        # Registering a expense
+
+        category = random.choice(expense_categories)
+        account = random.choice(accounts)
+        change = random.random() * random.choice([1, 10, 100, 1000]) + 0.01
+
+        ExpenseTransaction(
+            owner=main_user_id,
+            date_accomplished=current_day,
+            account_id=account.id,
+            change=-change,
+            category=category
+        ).save()
+
+
+        current_day = current_day + datetime.timedelta(days=random.randint(0, 1))
+
+
+def test_total_balance_between_funds_and_accounts(simulate_real_transactions, accounts, funds):
+    total_balance_funds = sum([fund.balance for fund in funds])
+    total_balance_accounts = sum([account.balance for account in accounts])
+
+    assert total_balance_accounts == total_balance_funds
+
