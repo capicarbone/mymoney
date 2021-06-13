59ae33cf16188061296ffa20a9fe1b5eda1324ae
Carlos Pinelly <cpinelly@gmail.com>
2021-06-13 16:07:40 -0500

    Added test for initial balance on account creation.

--- tests/functional/test_accounts_resource.py
+++ tests/functional/test_accounts_resource.py
@@ -1,3 +1,4 @@
+from .test_transaction_resource import resource_url as transactions_url
 
 resource_url = '/api/accounts'
 
@@ -22,12 +23,14 @@ def test_post_account_as_success(client, authenticated_header):
     json_data = rv.get_json()
     assert 'id' in json_data
 
+
 def test_post_account_invalid(client, authenticated_header):
     rv = client.post(resource_url, headers=authenticated_header, json={})
     assert rv.status_code == 400
 
     rv = client.post(resource_url, headers=authenticated_header, json={'name': ""})
     assert rv.status_code == 400
+
 
 def test_account_unauthorized_request(client):
     rv = client.get(resource_url)
@@ -37,3 +40,16 @@ def test_account_unauthorized_request(client):
     assert rv.status_code == 401
 
 
+def test_account_with_initial_balance(client, authenticated_header):
+    initial_balance = 2100
+    rv = client.post(resource_url, headers=authenticated_header,
+                     json={'name': 'New Account', 'initial_balance': initial_balance})
+    assert rv.status_code == 200
+
+    account_id = rv.get_json()['id']
+
+    rv = client.get(transactions_url, query_string={'account_id': account_id}, headers=authenticated_header)
+
+    items = rv.get_json()  # TODO extract from _items when pagination implemented
+    assert len(items) == 1
+    assert items[0]['account'] == account_id
